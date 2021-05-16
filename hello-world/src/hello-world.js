e6305f5d2f4faebc2f7542e5e58445b15fad303c
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-05-16 22:10:41 +0430

    module federation structure and setup

--- hello-world/src/hello-world.js
+++ hello-world/src/hello-world.js
@@ -2,12 +2,12 @@ import HelloWorldButton from './components/hello-world-button/hello-world-button
 import Heading from './components/heading/heading.js';
 
 const heading = new Heading();
-heading.render();
+heading.render('hello world');
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
 
 if (process.env.NODE_ENV === 'production') {
-  console.log('Production mode');
+    console.log('Production mode');
 } else if (process.env.NODE_ENV === 'development') {
-  console.log('Development mode');
+    console.log('Development mode');
 }
--- hello-world/src/server.js
+++ hello-world/src/server.js
@@ -3,17 +3,14 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 
-app.get('/hello-world', (req, res) => {
+app.get('/', function (req, res) {
   const pathToHtmlFile = path.resolve(__dirname, '../dist/hello-world.html');
   const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
   res.send(contentFromHtmlFile);
 });
-app.get('/kiwi', (req, res) => {
-  const pathToHtmlFile = path.resolve(__dirname, '../dist/kiwi.html');
-  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
-  res.send(contentFromHtmlFile);
-});
+
 app.use('/static', express.static(path.resolve(__dirname, '../dist')));
-app.listen(3000, () => {
-  console.log('Application is running on http://localhost:3000');
+
+app.listen(9001, function () {
+  console.log('Application is running on http://localhost:9001');
 });
