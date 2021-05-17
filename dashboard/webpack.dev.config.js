43b5dd63904226c211472715d6ed1de3981712d9
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-05-17 17:24:08 +0430

    navigation bar and micro frontend done

--- dashboard/webpack.dev.config.js
+++ dashboard/webpack.dev.config.js
@@ -31,6 +31,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
   plugins: [
