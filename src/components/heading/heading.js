29abf22936462b1e5e9eb9e88d5514398bf877dd
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-05-17 17:42:21 +0430

    rebuild heading with jquery

---
+++ src/components/heading/heading.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+
+import './heading.scss';
+
+class Heading {
+  render(pageName) {
+    const h1 = $('<h1>');
+    const body = $('body');
+    h1.text('Webpack is awesome. This is "' + pageName + '" page');
+    body.append(h1);
+  }
+}
+
+export default Heading;
