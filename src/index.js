f44dc6d1bbee14155250fa84c0d3b79f2cf86fd1
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-05-17 20:50:15 +0430

    fontawesome with webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
 import Heading from './components/heading/heading.js';
 import _ from 'lodash';
+
+library.add(faSpinner);
+dom.watch();
 
 const heading = new Heading();
 heading.render('hello world');
@@ -8,9 +13,9 @@ const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
 
 if (process.env.NODE_ENV === 'production') {
-    console.log('Production mode');
+  console.log('Production mode');
 } else if (process.env.NODE_ENV === 'development') {
-    console.log('Development mode');
+  console.log('Development mode');
 }
 
 console.log(_.upperFirst('index module'));
