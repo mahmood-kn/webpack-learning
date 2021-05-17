04087568e1ac700f45e52ab7d715a0e640575f8c
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-05-17 15:21:08 +0430

    make apps ready for micro frontend

--- kiwi/src/kiwi.js
+++ kiwi/src/kiwi.js
@@ -6,8 +6,8 @@ heading.render('kiwi');
 const kiwiImage = new KiwiImage();
 kiwiImage.render();
 
-import('HelloWorldApp/HelloWorldButton').then((HelloWorldButtonModule) => {
-  const HelloWorldButton = HelloWorldButtonModule.default;
-  const helloWorldButton = new HelloWorldButton();
-  helloWorldButton.render();
-});
+// import('HelloWorldApp/HelloWorldButton').then((HelloWorldButtonModule) => {
+//   const HelloWorldButton = HelloWorldButtonModule.default;
+//   const helloWorldButton = new HelloWorldButton();
+//   helloWorldButton.render();
+// });
